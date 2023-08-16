74a60abeec29d89da9c9eea4617ab4af224d85fe
Cuelho00 <lucascuelho00@gmail.com>
2023-08-16 03:25:08 -0300

    removed todo list

--- CPP_00/ex01/todo.cpp
+++
@@ -1,9 +0,0 @@
-
-
-//[] Create the cpp and hpp files for classes that I'm gonna need;
-//[] Create and treat the inputs that can be provided and what they'll do;
-//[] Design the display of the phonebook;
-//[] Take a look at the string functions like swapping and modifying/changing length,
-//I'm also thinking about displaying line by line, creating a function for the design
-//that gets the information but always displays the lines it will need.
-//[] Learn about private attributes
