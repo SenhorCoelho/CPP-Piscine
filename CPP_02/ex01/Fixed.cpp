0b2da41c93ae9723fa8e9a03f1093ea922e17afe
Cuelho00 <lucascuelho00@gmail.com>
2023-09-17 18:04:47 -0300

    A bit chaotic but working fine.

---
+++ CPP_02/ex01/Fixed.cpp
@@ -0,0 +1,71 @@
+#include "Fixed.hpp"
+
+//Constructor
+Fixed::Fixed(void) : _FPVal (0)
+{
+	return;
+}
+
+//Copy Constructor
+Fixed::Fixed(Fixed const &src)
+{
+	*this = src;
+	return;
+}
+
+//Const Int Constructot
+Fixed::Fixed(const int nbr)
+{
+	this->_FPVal = nbr * (1 << this->_FracBits);
+	return;
+}
+
+//Const Float Constructot
+Fixed::Fixed(const float nbr)
+{
+	this->_FPVal = roundf(nbr * double(1 << this->_FracBits));
+	return;
+}
+
+//Destructor
+Fixed::~Fixed(void)
+{
+	return;
+}
+
+//Copy Op. Overload
+Fixed & Fixed::operator=(Fixed const &rhs)
+{
+	if (this != &rhs)
+		this->_FPVal = rhs.getRawBits();
+
+	return *this;
+} 
+
+int Fixed::getRawBits(void) const
+{
+	return _FPVal;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_FPVal = raw;
+	return;
+}
+
+float Fixed::toFloat(void) const
+{
+	return float(_FPVal) / float(1 << this->_FracBits);
+}
+
+int Fixed::toInt(void) const
+{
+	return roundf(_FPVal / (1 << this->_FracBits));
+}
+
+//Operator << overload
+std::ostream &operator<<(std::ostream &o, Fixed const &src)
+{
+	o << src.toFloat();
+	return o;
+}
---
+++ CPP_02/ex01/Fixed.hpp
@@ -0,0 +1,31 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+	public:
+
+		Fixed(void);
+		Fixed(Fixed const &src);
+		Fixed(const int nbr);
+		Fixed(const float nbr);
+		~Fixed(void);
+		Fixed &operator=(Fixed const &rhs);
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+
+	private:
+
+		int	_FPVal;
+		static const int _FracBits = 8;
+
+};
+
+std::ostream &operator<<(std::ostream &o, Fixed const &src);
+
+#endif
